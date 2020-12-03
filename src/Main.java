debd2ed14ad6145f3fd2653fd7d415218f7812a4
Parth pahariya <wb-parthpaha447228@antfin.com>
2020-12-03 11:10:41 +0530

    main class

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String s = "UserName_yaduraj k_UserEmail_yadhu_r@yahoo.co.in_Phone_9844554101_OS_Web_TransactionID_160672666530_productName_MevoFit_Slim_HR_Activity_Tracker_Blue_Qty_1_cubeid_5ebe923d62534_Country_India_State_Karnataka_City_Bangalore_Pincode_560086";
+        System.out.println(s.length());
+
+    }
+
+}
